4de8c6cbbf9c0b905157ab20771ff80df99c0aa6
Caisha <Caisha@DESKTOP-814E0T9>
2025-06-25 14:22:38 -0400

    2005

--- calsss/Program.cs
+++ calsss/Program.cs
@@ -10,6 +10,22 @@ namespace calsss
     {
         static void Main(string[] args)
         {
+            string name = "Aisha omer"; 
+            Console.WriteLine("Name: " + name);
+            string mothername = "Xamdi Ali";
+            Console.WriteLine("Momother'Name: " + mothername);
+            string dop = "2000-05-16";
+            Console.WriteLine("Date Of Birth: " + dop);
+            string pob = "Galkio";
+            Console.WriteLine("Place Of Pirt: " + pob);
+            string address = "Garsoor";
+            Console.WriteLine("Address: " + address);
+            int telphone = 097765567;
+            Console.WriteLine("Telphone: " + telphone);
+            string sex = "Female";
+            Console.WriteLine("Sex: " + sex);
+            string status = "Single";
+            Console.WriteLine("Status: " + status);
         }
     }
 }
