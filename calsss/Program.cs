3387c830d0e8d4251babe8884ff1f191821820ce
Caisha <Caisha@DESKTOP-814E0T9>
2025-07-05 09:15:50 +0300

    main project

--- calsss/Program.cs
+++ calsss/Program.cs
@@ -4,33 +4,80 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace calsss
+namespace rrtttefghj
 {
     internal class Program
     {
         static void Main(string[] args)
+
         {
-            string name = "Aisha omer abdi";
-            Console.WriteLine("Name: " + name);
-            string mothername = "Xamdi Ali";
-            Console.WriteLine("Momother'Name: " + mothername);
+            Console.Write("Fadlan geli magacaaga: ");
+            string studentName = Console.ReadLine();
 
-            int birthYear = 2000;
-            int birthMonth = 05;
-            int birthDay = 15;
-            DateTime dob = new DateTime(birthYear, birthMonth, birthDay);
-            Console.WriteLine("Date Of Birth: " + dob.ToString("yyyy-MM-dd"));
+            Console.Write("Fadlan geli ID-gaaga: ");
+            string studentID = Console.ReadLine();
 
-            string pob = "Galkio";
-            Console.WriteLine("Place Of Pirth: " + pob);
-            string address = "Garsoor";
-            Console.WriteLine("Address: " + address);
-            int telphone = 097765567;
-            Console.WriteLine("Telphone: " + telphone);
-            string sex = "Female";
-            Console.WriteLine("Sex: " + sex);
-            string status = "Single";
-            Console.WriteLine("Status: " + status);
+            Console.WriteLine("\n== Xulo nooca xisaabta ==\n1. Kudar\n2. Kajar\n3. Kudhufo\n4. Qaybi");
+            Console.Write("Xulashadaada (1-4): ");
+            int selection = Convert.ToInt32(Console.ReadLine());
+
+            int trueAnswers = 0;
+            int totalQuestions = 10;
+
+            Random generator = new Random();
+
+            for (int num = 1; num <= totalQuestions; num++)
+            {
+                int x = generator.Next(1, 20);
+                int y = generator.Next(1, 20);
+                int correctAnswer = 0;
+
+                switch (selection)
+                {
+                    case 1:
+                        Console.Write($"Q{num}: {x} + {y} = ");
+                        correctAnswer = x + y;
+                        break;
+                    case 2:
+                        Console.Write($"Q{num}: {x} - {y} = ");
+                        correctAnswer = x - y;
+                        break;
+                    case 3:
+                        Console.Write($"Q{num}: {x} * {y} = ");
+                        correctAnswer = x * y;
+                        break;
+                    case 4:
+                        x = x * y;
+                        Console.Write($"Q{num}: {x} / {y} = ");
+                        correctAnswer = x / y;
+                        break;
+                    default:
+                        Console.WriteLine("Xulashada khaldan!");
+                        return;
+                }
+
+                int userAnswer = Convert.ToInt32(Console.ReadLine());
+
+                if (userAnswer == correctAnswer)
+                {
+                    Console.WriteLine("👏 Waa sax!\n");
+                    trueAnswers++;
+                }
+                else
+                {
+                    Console.WriteLine($"❌ Waa khalad! Jawaabta saxda ahayd waa: {correctAnswer}\n");
+                }
+            }
+
+            int incorrect = totalQuestions - trueAnswers;
+            int percentage = (trueAnswers * 100) / totalQuestions;
+
+            Console.WriteLine("\n--- Natiijadaada ---");
+            Console.WriteLine($"Magaca: {studentName}");
+            Console.WriteLine($"ID-ga: {studentID}");
+            Console.WriteLine($"Sax: {trueAnswers} | Khalad: {incorrect}");
+            Console.WriteLine($"Guud ahaan: {percentage}%");
+
         }
     }
 }
